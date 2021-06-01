ef1e93a7f3bdf6992de8255c604627f275ddbefa
Bhoomit <bhoomit@gmail.com>
2021-06-01 21:04:51 +0530

    Added object.freeze method

--- src/utils/object.js
+++ src/utils/object.js
@@ -21,4 +21,19 @@ export const object = [
       'you get 2 in return which is length of object',
     ],
   },
+  {
+    name: 'freeze',
+    description: [
+      'This method returns an objects whose values can not be changes',
+      'suppose if we create const person = { name: "test"  } still we can change person.name = "another" ',
+      'but this method returns an object whose values can not be changed',
+      'Usage:',
+      'obj.length(obj)',
+      'you pas => obj.length({',
+      'name: "test"',
+      'age: "30"',
+      '})',
+      'you get 2 in return which is length of object',
+    ],
+  },
 ]
