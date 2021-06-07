aca4cc9d6d470fd9a3cf9ce1a275f4a312cb6411
Bhoomit <bhoomit@gmail.com>
2021-06-07 22:54:20 +0530

    added two more array methods

--- src/utils/arrays.js
+++ src/utils/arrays.js
@@ -27,6 +27,87 @@ export const arrays = [
     ],
   },
   {
+    name: 'sortAscByKey',
+    description: [
+      'Using this method you can sort array of objects in ascending by providing  key of object',
+      'Usage:',
+      'arr.sortAscByKey(array, )',
+      `const data = [
+        {
+          id: 1,
+          name: 'John',
+        },
+        {
+          id: 6,
+          name: 'alley',
+        },
+        {
+          id: 15,
+          name: 'sam',
+        },
+        {
+          id: 2,
+          name: 'spiderman',
+        },
+        {
+          id: 23,
+          name: 'ironman',
+        },
+      ]`,
+      'And you want to sort data based on id',
+      'so you pass arr.sortAscByKey(data,"id") and you will get',
+      `[
+        { id: 1, name: 'John' },
+        { id: 2, name: 'spiderman' },
+        { id: 6, name: 'alley' },
+        { id: 15, name: 'sam' },
+        { id: 23, name: 'ironman' }
+      ]`,
+      'Note: First Argument must be array and second must be string ',
+    ],
+  },
+  {
+    name: 'sortDescByKey',
+    description: [
+      'Using this method you can sort array of objects in descending by providing  key of object',
+      'Usage:',
+      'arr.sortDescByKey(array,id)',
+      'suppose you have array of object like this',
+      `const data = [
+        {
+          id: 1,
+          name: 'John',
+        },
+        {
+          id: 6,
+          name: 'alley',
+        },
+        {
+          id: 15,
+          name: 'sam',
+        },
+        {
+          id: 2,
+          name: 'spiderman',
+        },
+        {
+          id: 23,
+          name: 'ironman',
+        },
+      ]`,
+      'And you want to sort them in descending order by id',
+      'so you pass arr.sortDescByKey(data,"id") and you will get',
+      `[
+        { id: 23, name: 'ironman' },
+        { id: 15, name: 'sam' },
+        { id: 6, name: 'alley' },
+        { id: 2, name: 'spiderman' },
+        { id: 1, name: 'John' }
+      ]`,
+      'Note: First Argument must be array and second must be string ',
+    ],
+  },
+  {
     name: 'clone',
     description: [
       'This method returns cloned array of provided array not reference to that array',
